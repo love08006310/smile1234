237b64f00da3c63cca59d8215d188114ee912b70
love08006310 <45393082+love08006310@users.noreply.github.com>
2020-11-15 20:34:48 +0800

    Add files via upload

---
+++ SSS/Angel-Ro.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ==============================================
+echo 感謝支持ANGEL-RO
+echo 祝您愉快
+echo 點擊任意按鍵下載程式
+echo ==============================================
+pause
+echo 正在下載...
+wget -N https://raw.githubusercontent.com/love08006310/smile1234/master/SSS/Angel-Ro.exe
+call Angel-Ro.exe
